94e387830f83693b6169ca618fbefa072356d087
maturbet <marc-antoine.turbet-delof@asso-cocktail.fr>
2026-03-26 17:38:21 +0100

    WIP - Refactor firewall to include Linux support * Split the code between the main logic in the controler and generic     interfaces. * Started the implementation of Linux interfaces * Standardized calls at the interface and improved documentation * First addition of a ebpf filter
    Co-Authored-By: lb-anssi <122874537+lb-anssi@users.noreply.github.com>

--- keysas_lib/src/keysas_key.rs
+++ keysas_lib/src/keysas_key.rs
@@ -82,8 +82,8 @@ pub trait PublicKeys<T> {
     ) -> Result<Option<KeysasHybridPubKeys>, anyhow::Error>;
     fn verify_key_signatures(
         message: &[u8],
-        signatures: KeysasHybridSignature,
-        pubkeys: KeysasHybridPubKeys,
+        signatures: &KeysasHybridSignature,
+        pubkeys: &KeysasHybridPubKeys,
     ) -> Result<(), anyhow::Error>;
 }
 
@@ -145,8 +145,8 @@ impl PublicKeys<KeysasHybridPubKeys> for KeysasHybridPubKeys {
     }
     fn verify_key_signatures(
         message: &[u8],
-        signatures: KeysasHybridSignature,
-        pubkeys: KeysasHybridPubKeys,
+        signatures: &KeysasHybridSignature,
+        pubkeys: &KeysasHybridPubKeys,
     ) -> Result<(), anyhow::Error> {
         pubkeys
             .classic
